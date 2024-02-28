397371fc869dc7673b6630e3e680476550f7e301
Anas WS <anas@wednesday.is>
2024-02-28 15:47:56 +0530

    Audio play option added

--- app/components/TrackCard/index.js
+++ app/components/TrackCard/index.js
@@ -4,7 +4,7 @@
  *
  */
 
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Card } from '@mui/material';
 import T from '@components/T';
@@ -16,8 +16,13 @@ import CardMedia from '@mui/material/CardMedia';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import PlayArrowIcon from '@mui/icons-material/PlayArrow';
+import PauseIcon from '@mui/icons-material/Pause';
 
-export function TrackCard({ collectionName, artistName, shortDescription, artworkUrl100 }) {
+export function TrackCard({ collectionName, artistName, shortDescription, artworkUrl100, previewUrl }) {
+  const [playTrack, setPlayTrack] = useState(false);
+  const shortDesc =
+    shortDescription && shortDescription.length > 38 ? `${shortDescription.substring(0, 38)}...` : shortDescription;
+
   return (
     <Card
       sx={{
@@ -34,9 +39,13 @@ export function TrackCard({ collectionName, artistName, shortDescription, artwor
       <Box sx={{ display: 'flex', flexDirection: 'column' }}>
         <CardContent sx={{ flex: '1 0 auto' }}>
           <Box sx={{ display: 'flex', alignItems: 'center', pb: 1 }}>
-            <IconButton aria-label="play/pause">
-              <PlayArrowIcon sx={{ height: 32, width: 32 }} />
-            </IconButton>
+            <If condition={!isEmpty(previewUrl)}>
+              <IconButton aria-label="play/pause" onClick={() => setPlayTrack(!playTrack)}>
+                <If condition={playTrack} otherwise={<PlayArrowIcon sx={{ height: 32, width: 32 }} />}>
+                  <PauseIcon sx={{ height: 32, width: 32 }} />
+                </If>
+              </IconButton>
+            </If>
             <Typography component="div" variant="h5">
               {collectionName.length > 18 ? `${collectionName.substring(0, 18)}...` : collectionName}
             </Typography>
@@ -53,11 +62,16 @@ export function TrackCard({ collectionName, artistName, shortDescription, artwor
             condition={!isEmpty(shortDescription)}
             otherwise={<T data-testid="track_shortdesc_unavailable" id="track_shortdesc_unavailable" />}
           >
-            <T data-testid="track_shortdesc" id="track_shortdesc" values={{ desc: shortDescription }} />
+            <T data-testid="track_shortdesc" id="track_shortdesc" values={{ desc: shortDesc }} />
           </If>
         </CardContent>
       </Box>
-      <CardMedia component="img" sx={{ width: 151 }} image={artworkUrl100} alt="Live from space album cover" />
+      <CardMedia component="img" sx={{ width: 151 }} image={artworkUrl100} alt={collectionName} />
+      <If condition={playTrack}>
+        <audio autoPlay name="media">
+          <source src={previewUrl} />
+        </audio>
+      </If>
     </Card>
   );
 }
@@ -66,7 +80,8 @@ TrackCard.propTypes = {
   artistName: PropTypes.string,
   artworkUrl100: PropTypes.string,
   collectionName: PropTypes.string,
-  shortDescription: PropTypes.string
+  shortDescription: PropTypes.string,
+  previewUrl: PropTypes.string
 };
 
 export default TrackCard;
